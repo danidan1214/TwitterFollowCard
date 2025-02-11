bcb21f038cb152a3b9d96710eb68ff11225e05c6
Daniel Palma <danieljpm554@gmail.com>
2025-02-11 15:48:53 -0500

    Folow card state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import { TwitterFollowCard } from "./assets/TwitterFollowCard.jsx";
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 
 export function App() {
   return (
     <div className="App">
-      <TwitterFollowCard userName="danidan1214" isFollowing>
+      <TwitterFollowCard userName="danidan1214" initialIsFollowing={true}>
         Daniel Palma
       </TwitterFollowCard>
-      <TwitterFollowCard userName="midudev" isFollowing={false}>
+      <TwitterFollowCard userName="midudev">
         Miguel Ángel Durán
       </TwitterFollowCard>
-      <TwitterFollowCard userName="midudev" isFollowing={false}>
+      <TwitterFollowCard userName="midudev">
         Miguel Ángel Durán
       </TwitterFollowCard>
     </div>
